27ec6031af49528726f0bd1f7038a3b4e18efc02
adam <adam>
2008-07-07 09:24:35 +0000

    more log tweaks

--- lib/personal-mail.sh
+++ lib/personal-mail.sh
@@ -2,6 +2,7 @@
 
 export MAIL_PROFILE=personal
 export MAIL_PROFILE_DIR=$HOME/mail/$MAIL_PROFILE
+export MAIL_PROFILE_LOG=$MAIL_PROFILE_DIR/log
 
 export MAIRIX_DIR=$HOME/.mairix/$MAIL_PROFILE
 export MAIRIXRC=$MAIRIX_DIR/config
@@ -9,7 +10,9 @@ export MAIRIX_RESULTS=$HOME/mail/$MAIL_PROFILE/mairix
 export MAIRIX_VERSION=$( mairix --version | awk '{print $2}' )
 
 export OFFLINEIMAP_CONF=$MAIL_PROFILE_DIR/.offlineimaprc
-export OFFLINEIMAP_LOG_DIR=$MAIL_PROFILE_DIR/log/offlineimap/`date +%Y/%m`
+export OFFLINEIMAP_LOG_DIR=$MAIL_PROFILE_LOG/offlineimap/`date +%Y/%m`
 export OFFLINEIMAP_LOG=$OFFLINEIMAP_LOG_DIR/`date +%d`
-export OFFLINEIMAP_LOG_DEBUG=$OFFLINEIMAP_LOG-debug
+export OFFLINEIMAP_LOG_DEBUG=${OFFLINEIMAP_LOG}-debug
 [ -d "$OFFLINEIMAP_LOG_DIR" ] || mkdir -p "$OFFLINEIMAP_LOG_DIR"
+
+export MAIL_DAEMON_LOG=$MAIL_PROFILE_LOG/daemon.log
