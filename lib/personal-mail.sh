8f67d1251226a4b26aecb840298bde7d9b8792ff
adam <adam>
2008-07-07 09:18:32 +0000

    change log dir

--- lib/personal-mail.sh
+++ lib/personal-mail.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
 export MAIL_PROFILE=personal
+export MAIL_PROFILE_DIR=$HOME/mail/$MAIL_PROFILE
+
 export MAIRIX_DIR=$HOME/.mairix/$MAIL_PROFILE
 export MAIRIXRC=$MAIRIX_DIR/config
 export MAIRIX_RESULTS=$HOME/mail/$MAIL_PROFILE/mairix
 export MAIRIX_VERSION=$( mairix --version | awk '{print $2}' )
+
+export OFFLINEIMAP_CONF=$MAIL_PROFILE_DIR/.offlineimaprc
+export OFFLINEIMAP_LOG_DIR=$MAIL_PROFILE_DIR/log/offlineimap/`date +%Y/%m`
+export OFFLINEIMAP_LOG=$OFFLINEIMAP_LOG_DIR/`date +%d`
+export OFFLINEIMAP_LOG_DEBUG=$OFFLINEIMAP_LOG-debug
+[ -d "$OFFLINEIMAP_LOG_DIR" ] || mkdir -p "$OFFLINEIMAP_LOG_DIR"
