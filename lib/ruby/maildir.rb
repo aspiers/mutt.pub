16b139f3ea0a5d1e4c94326b5eb3059e3cfc0c5e
adam <adam>
2010-09-06 14:01:25 +0000

    support creating maildirs from MailDirPathError exception

--- lib/ruby/maildir.rb
+++ lib/ruby/maildir.rb
@@ -1,11 +1,13 @@
 #!/usr/bin/ruby
 
+SUBDIRS = %w(cur new tmp)
+
 class MailDir
   attr_reader :path
 
   def initialize(path)
     @path = path
-    %w(cur new tmp).each do |subdir|
+    SUBDIRS.each do |subdir|
       dir = File.join(@path, subdir)
       unless FileTest.directory?(dir)
         raise MailDirPathError.new(@path, subdir), "#{dir} not a directory"
@@ -34,5 +36,15 @@ class MailDirPathError < RuntimeError
   def initialize(dir, missing)
     @dir = dir
     @missing = missing
+  end
+
+  def create_maildir
+    SUBDIRS.each do |subdir|
+      path = File.join(@dir, subdir)
+      unless FileTest.directory?(path)
+        Pathname.new(path).mkpath
+        puts "created " + path
+      end
+    end
   end
 end
