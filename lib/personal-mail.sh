c45c054b83ec420c90319e09f85f774fc6edf27c
Adam Spiers <dotfiles@adamspiers.org>
2013-01-06 12:09:19 +0000

    dedupe archives too

--- lib/personal-mail.sh
+++ lib/personal-mail.sh
@@ -2,7 +2,11 @@
 
 export MAIL_PROFILE=personal
 export OFFLINEIMAP_SERVER=imap.googlemail.com
+# export LDAP_SERVER=fixme
+# export LDAP_BASE=fixme
 
 . ~/lib/common-mail.sh
 
-export OFFLINEIMAP_DEDUPE="$OFFLINEIMAP_INBOX"
+OFFLINEIMAP_CURRENT_MONTH=$MAIL_PROFILE_DIR/archive/gm/`date +'%Y-%m'`
+#OFFLINEIMAP_PREV_MONTH=$MAIL_PROFILE_DIR/archive/gm/`date +'%Y-%m' -d "$(date +%Y-%m-15) -1 month"`
+export OFFLINEIMAP_DEDUPE="$OFFLINEIMAP_INBOX $OFFLINEIMAP_CURRENT_MONTH"
